02b75b7a320f0adf81cd365a8670dfd4774cbf57
Scott Francis <scott@kurokoproject.com>
2019-03-18 17:22:24 -0700

    'output' should be the root server directory

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
 require 'webrick'
 
-# Simplistic server that just serves the root of where it's at.
+# Simplistic server that just serves ./output
 # Meant for static website debugging
 
-root = File.expand_path(File.dirname(__FILE__))
+root = File.join(File.expand_path(File.dirname(__FILE__)), "output")
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 trap 'INT' do server.shutdown end
 
